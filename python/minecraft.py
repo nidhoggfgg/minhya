b32aa1a5236699762275390c27693a3be9b786a8
nidhoggfgg <nidhoggfgg@gmail.com>
2024-03-06 21:16:10 +0800

    minecraft support

--- python/minecraft.py
+++ python/minecraft.py
@@ -1,65 +1,159 @@
 from PIL import Image
 import numpy as np
-
-PIC_XZ = "" # put the 1st picture path here
-PIC_YZ = "" # put the 2nd picture path here
-THRESHOLD = 180 # set the threshold here, normally 180 is good
-CUT = True # cut the internal dots or not
-BLOCK = "minecraft:white_wool" # the block in minecraft
+import argparse
+import os
 
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        description="Make a datapack to put 3D object build from 2 image for Minecraft"
+    )
+    parser.add_argument(
+        "img1", metavar="img1", type=str, help="the path of the 1st image"
+    )
+    parser.add_argument(
+        "img2", metavar="img2", type=str, help="the path of the 2nd image"
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        type=int,
+        default=7,
+        help="the version of the datapack default is 7",
+    )
+    parser.add_argument(
+        "-p",
+        "--path",
+        type=str,
+        default="minhya",
+        help='the path of the datapack default is "minhya"',
+    )
+    parser.add_argument(
+        "-n",
+        "--namespace",
+        type=str,
+        default="minhya",
+        help='the namespace of the datapack default is "minhya"',
+    )
+    parser.add_argument(
+        "-b",
+        "--block",
+        type=str,
+        default="minecraft:white_wool",
+        help='the block in minecraft default is "minecraft:white_wool"',
+    )
+    parser.add_argument(
+        "-t",
+        "--threshold",
+        type=int,
+        default=180,
+        help="the threshold of the image (0-255) default is 180",
+    )
+    parser.add_argument(
+        "-c", "--cut", action="store_true", help="cut the internal dots"
+    )
+    parser.add_argument(
+        "--batch",
+        action="store_true",
+        help="use batch to avoid command limit and support BE",
+    )
+    args = parser.parse_args()
+    obj = make_obj(args.img1, args.img2, args.threshold, args.cut)
+    make_datapack(
+        obj,
+        args.block,
+        args.path,
+        args.namespace,
+        args.version,
+        args.batch,
+    )
 
 
-def make_datapack(obj, path, namespace, name, undo):
+def make_datapack(obj, block, path, namespace, version, batch):
+    os.makedirs(f"{path}/data/{namespace}/functions", exist_ok=True)
+    write_pack_mcmeta(path, version)
+    write_functions(obj, path, namespace, block, batch)
+
+
+def write_pack_mcmeta(path, version):
+    with open(f"{path}/pack.mcmeta", "w") as f:
+        f.write(
+            f"""{{
+  "pack": {{
+    "pack_format": {version},
+    "description": "3D object made from 2 images"
+  }}
+}}"""
+        )
+
+
+def write_functions(obj, path, namespace, block, batch):
+    draw, undo = "draw", "undo"
     obj = np.array(obj)
-    obj[:, [1, 2]] = obj[:, [2, 1]] # y->z, z->y
 
-    # batches, because there is a limit of commands per function
-    batch_size = 10000
-    batch_num = 1 + (len(obj) - 1) // batch_size
-    batches = np.array_split(obj, batch_num)
+    if batch:
+        # batches, because there is a limit of commands per function
+        batch_size = 10000
+        batch_num = 1 + (len(obj) - 1) // batch_size
+        batches = np.array_split(obj, batch_num)
 
-    # write both the draw and undo functions
-    for i, batch in enumerate(batches):
-        with open(f"{path}/{name}{i}.mcfunction", "w") as f1, open(
-            f"{path}/{undo}{i}.mcfunction", "w"
+        # write both the draw and undo sub-functions
+        for i, batch in enumerate(batches):
+            with open(
+                f"{path}/data/{namespace}/functions/{draw}{i}.mcfunction", "w"
+            ) as f1, open(
+                f"{path}/data/{namespace}/functions/{undo}{i}.mcfunction", "w"
+            ) as f2:
+                for point in batch:
+                    x, y, z = point[0], point[1], point[2]
+                    block_command = f"setblock {x} {y} {z} {block}\n"
+                    undo_command = f"setblock {x} {y} {z} minecraft:air\n"
+                    f1.write(block_command)
+                    f2.write(undo_command)
+
+        # write the main function
+        with open(
+            f"{path}/data/{namespace}/functions/{draw}.mcfunction", "w"
+        ) as f1, open(
+            f"{path}/data/{namespace}/functions/{undo}.mcfunction", "w"
         ) as f2:
-            for point in batch:
+            f1.write(f"gamerule maxCommandChainLength 1000000\n")
+            f2.write(f"gamerule maxCommandChainLength 1000000\n")
+            for i in range(len(batches)):
+                f1.write(f"function {namespace}:{draw}{i}\n")
+                f2.write(f"function {namespace}:{undo}{i}\n")
+    else:
+        with open(
+            f"{path}/data/{namespace}/functions/{draw}.mcfunction", "w"
+        ) as f1, open(
+            f"{path}/data/{namespace}/functions/{undo}.mcfunction", "w"
+        ) as f2:
+            f1.write(f"gamerule commandModificationBlockLimit 1000000\n")
+            f2.write(f"gamerule commandModificationBlockLimit 1000000\n")
+            for point in obj:
                 x, y, z = point[0], point[1], point[2]
-                block_command = f"setblock {x} {y} {z} {BLOCK}\n"
+                block_command = f"setblock {x} {y} {z} {block}\n"
                 undo_command = f"setblock {x} {y} {z} minecraft:air\n"
                 f1.write(block_command)
                 f2.write(undo_command)
-    
-    # write the main function
-    with open(f"{path}/{name}.mcfunction", "w") as f1, open(
-        f"{path}/{undo}.mcfunction", "w"
-    ) as f2:
-        for i in range(len(batches)):
-            f1.write(f"function {namespace}:{name}{i}\n")
-            f2.write(f"function {namespace}:{undo}{i}\n")
-     
-
-# this function isn't used, we use obj[:, [1, 2]] = obj[:, [2, 1]] instead
-# but if you really want to rotate rather than change the axis, use this
-def rotate_90(point):
-    theta = np.pi / 2
-    rotation_matrix = np.array(
-        [
-            [1, 0, 0],
-            [0, np.cos(theta), -np.sin(theta)],
-            [0, np.sin(theta), np.cos(theta)],
-        ]
-    )
-
-    rotated_point = np.dot(rotation_matrix, point)
-    return rotated_point
 
 
-def scale(img, f):
-    return img.resize((int(img.width * f), int(img.height * f)))
+def make_obj(img1, img2, threshold, cut):
+    img1 = pre_resize(img1, threshold)
+    img2 = pre_resize(img2, threshold)
+    img1, img2 = auto_resize(img1, img2)
+    if img1.height > 255:
+        img1 = scale(img1)
+        img2 = scale(img2)
+    obj = build_unchecked(img1, img2)
+    if cut:
+        obj = cut(obj)
+    return obj
+
+
+def scale(img):
+    f = 255 / img.height
+    return img.resize((int(img.width * f), 255))
 
 
 def pre_resize(pic, threshold):
@@ -102,10 +196,6 @@ def auto_resize(img1, img2):
 
 
 def build_unchecked(img1, img2):
-    """
-    make the object
-    return [(x, y, z), ...]
-    """
     assert img1.height == img2.height
     height = img1.height
     img_xz = np.array(img1)
@@ -124,7 +214,7 @@ def build_unchecked(img1, img2):
                 if y != 255:
                     # z-axis in the image is facing down,
                     # so change the z-axis so that it is facing up
-                    obj.append((ix, iy, -iz + height))
+                    obj.append((ix, -iz + height, iy))
     return obj
 
 
@@ -159,4 +249,3 @@ def cut(obj):
 
 if __name__ == "__main__":
     main()
-
